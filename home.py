4af46904668866f12d31bf8d2edb19824ad8f314
coolavy <avyangupta@gmail.com>
2024-02-20 18:16:22 -0500

    First Piece of Progress

--- home.py
+++ home.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
-st.title("James Weldon Johnson")
-st.subheader("")
+# Custom styled title
+new_title = "<div class=\"container\">"\
+            "<div class=\"text\">"\
+            "<p>"\
+            "<span style=\"font-family:sans-serif; color:#27B47A;font-size:60px;\"><b>James</b></span>"\
+            "<span style=\"font-family:sans-serif; color:#27B47A;font-size:60px;\"><b> Weldon Johnson</b></span>"\
+            "</p>"\
+            "</div>"\
+            "</div>"
 
-st.markdown("")
+# Display the custom title
+st.markdown(new_title, unsafe_allow_html=True)
+
+# Image and caption
+col1, _ = st.columns([1, 2])
+with col1:
+    st.image("images/james-weldon-johnson.jpg", width=435, caption='James Weldon Johnson')
---
+++ pages/1_Works.py
@@ -0,0 +1,155 @@
+import streamlit as st
+
+# Custom styled title
+new_title = "<div class=\"container\">"\
+            "<div class=\"text\">"\
+            "<p>"\
+            "<span style=\"font-family:sans-serif; color:#27B47A;font-size:45px;\"><b>Works</b></span>"\
+            "</p>"\
+            "</div>"\
+            "</div>"
+
+# Display the custom title
+st.markdown(new_title, unsafe_allow_html=True)
+
+st.subheader("Lift Every Voice and Sing")
+
+poem1 = """
+Lift every voice and sing\n
+Till earth and heaven ring,\n
+Ring with the harmonies of Liberty;\n
+Let our rejoicing rise\n
+High as the listening skies,\n
+Let it resound loud as the rolling sea.\n
+Sing a song full of the faith that the dark past has taught us,\n
+Sing a song full of the hope that the present has brought us.\n
+Facing the rising sun of our new day begun,\n
+Let us march on till victory is won.\n
+Stony the road we trod,\n
+Bitter the chastening rod,\n
+Felt in the days when hope unborn had died;\n
+Yet with a steady beat,\n
+Have not our weary feet\n
+Come to the place for which our fathers sighed?\n
+We have come over a way that with tears has been watered,\n
+We have come, treading our path through the blood of the slaughtered,\n
+Out from the gloomy past,\n
+Till now we stand at last\n
+Where the white gleam of our bright star is cast.\n\n
+God of our weary years,\n
+God of our silent tears,\n
+Thou who hast brought us thus far on the way;\n
+Thou who hast by Thy might\n
+Led us into the light,\n
+Keep us forever in the path, we pray.\n
+Lest our feet stray from the places, our God, where we met Thee,\n
+Lest, our hearts drunk with the wine of the world, we forget Thee;\n
+Shadowed beneath Thy hand,\n
+May we forever stand.\n
+True to our God,\n
+True to our native land.\n
+"""
+
+st.markdown(poem1)
+
+st.divider()
+
+st.subheader("The Creation")
+
+poem2 = """
+And God stepped out on space,\n
+And he looked around and said:\n
+I'm lonely—\n
+I'll make me a world.\n//\n
+And far as the eye of God could see\n
+Darkness covered everything,\n
+Blacker than a hundred midnights\n
+Down in a cypress swamp.\n//\n
+Then God smiled,\n
+And the light broke,\n
+And the darkness rolled up on one side,\n
+And the light stood shining on the other,\n
+And God said: That's good!\n//\n
+Then God reached out and took the light in his hands,\n
+And God rolled the light around in his hands\n
+Until he made the sun;\n
+And he set that sun a-blazing in the heavens.\n
+And the light that was left from making the sun\n
+God gathered it up in a shining ball\n
+And flung it against the darkness,\n
+Spangling the night with the moon and stars.\n
+Then down between\n
+The darkness and the light\n
+He hurled the world;\n
+And God said: That's good!\n//\n
+Then God himself stepped down—\n
+And the sun was on his right hand,\n
+And the moon was on his left;\n
+The stars were clustered about his head,\n
+And the earth was under his feet.\n
+And God walked, and where he trod\n
+His footsteps hollowed the valleys out\n
+And bulged the mountains up.\n//\n
+Then he stopped and looked and saw\n
+That the earth was hot and barren.\n
+So God stepped over to the edge of the world\n
+And he spat out the seven seas—\n
+He batted his eyes, and the lightnings flashed—\n
+He clapped his hands, and the thunders rolled—\n
+And the waters above the earth came down,\n
+The cooling waters came down.\n//\n
+Then the green grass sprouted,\n
+And the little red flowers blossomed,\n
+The pine tree pointed his finger to the sky,\n
+And the oak spread out his arms,\n
+The lakes cuddled down in the hollows of the ground,\n
+And the rivers ran down to the sea;\n
+And God smiled again,\n
+And the rainbow appeared,\n
+And curled itself around his shoulder.\n//\n
+Then God raised his arm and he waved his hand\n
+Over the sea and over the land,\n
+And he said: Bring forth! Bring forth!\n
+And quicker than God could drop his hand,\n
+Fishes and fowls\n
+And beasts and birds\n
+Swam the rivers and the seas,\n
+Roamed the forests and the woods,\n
+And split the air with their wings.\n
+And God said: That's good!\n//\n
+Then God walked around,\n
+And God looked around\n
+On all that he had made.\n
+He looked at his sun,\n
+And he looked at his moon,\n
+And he looked at his little stars;\n
+He looked on his world\n
+With all its living things,\n
+And God said: I'm lonely still.\n//\n
+Then God sat down—\n
+On the side of a hill where he could think;\n
+By a deep, wide river he sat down;\n
+With his head in his hands,\n
+God thought and thought,\n
+Till he thought: I'll make me a man!\n//\n
+Up from the bed of the river\n
+God scooped the clay;\n
+And by the bank of the river\n
+He kneeled him down;\n
+And there the great God Almighty\n
+Who lit the sun and fixed it in the sky,\n
+Who flung the stars to the most far corner of the night,\n
+Who rounded the earth in the middle of his hand;\n
+This great God,\n
+Like a mammy bending over her baby,\n
+Kneeled down in the dust\n
+Toiling over a lump of clay\n
+Till he shaped it in is his own image;\n//\n
+Then into it he blew the breath of life,\n
+And man became a living soul.\n
+Amen.\n//\n
+\nAmen.
+"""
+
+st.markdown(poem2)
+st.divider()
